14f690fb84914406e0d1f2452e8368c7023c882f
CanardConfit <canardconfit.developpement@gmail.com>
2024-05-16 22:48:45 +0200

    Add OGP tags and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,21 @@ export default defineNuxtConfig({
   devtools: {enabled: true},
   app: {
     head: {
+      title: "CC Compiler",
       meta: [
-        {charset: 'utf-8'},
-        {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { key: 'og:title', property: 'og:title', content: 'CC Compiler' },
+        { key: 'og:description', property: 'og:description', content: 'CC Compiler - A custom compiler project for HEPIA students to compile C-like for their own processor.' },
+        { key: 'og:image', property: 'og:image', content: '/favicon.ico' },
+        { key: 'og:url', property: 'og:url', content: 'https://cc-compiler.canardconfit.ch' },
+        { key: 'og:type', property: 'og:type', content: 'website' },
+        { key: 'twitter:title', name: 'twitter:title', content: 'CC Compiler' },
+        { key: "twitter:domain", property: "twitter:domain", content: "cc-compiler.canardconfit.ch" },
+        { key: "twitter:url", property: "twitter:url", content: "https://cc-compiler.canardconfit.ch" },
+        { key: 'twitter:description', name: 'twitter:description', content: 'CC Compiler - A custom compiler project for HEPIA students to compile C-like for to create their own processor.' },
+        { key: 'twitter:image', name: 'twitter:image', content: '/favicon.ico' },
+        { key: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
       ],
     }
   },
